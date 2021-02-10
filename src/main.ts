89a454a2cffc2bdb3a2532842d4e71a7e0a60449
박진용 <jinyoung1997@gmail.com>
2021-02-10 14:47:59 +0900

    chore : add cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-
+  app.use(cookieParser());
   await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
