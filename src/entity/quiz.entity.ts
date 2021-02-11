c782c730435355f7d6fe05499eca0a6989326f67
swimme <2sy100@likelion.org>
2021-02-12 02:05:47 +0900

    fix: quiz 엔티티 quizBookId 컬럼수정

--- src/entity/quiz.entity.ts
+++ src/entity/quiz.entity.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 import { QuizBookEntity } from './quiz-book.entity';
 import { UserSolveQuizBookEntity } from './user-solve-quiz-book.entity';
@@ -8,38 +15,37 @@ export class QuizEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'varchar', length: 225,nullable: false})
-  question:string;
+  @Column({ type: 'varchar', length: 225, nullable: false })
+  question: string;
 
-  @Column({type: 'text', nullable: false})
-  answer:string;
+  @Column({ type: 'text', nullable: false })
+  answer: string;
 
-  @Column({ type: 'text', nullable: false})
-  imageURL:string;
+  @Column({ type: 'text', nullable: false })
+  imageURL: string;
 
-  @Column({type: 'text', nullable: true})
-  option1:string;
+  @Column({ type: 'text', nullable: true })
+  option1: string;
 
-  @Column({type: 'text', nullable: true})
-  option2:string;
+  @Column({ type: 'text', nullable: true })
+  option2: string;
 
-  @Column({type: 'text', nullable: true})
-  option3:string;
+  @Column({ type: 'text', nullable: true })
+  option3: string;
 
-  @Column({type: 'text', nullable: true})
-  option4:string;
+  @Column({ type: 'text', nullable: true })
+  option4: string;
 
-  @Column({type:'text', nullable: true})
+  @Column({ type: 'text', nullable: true })
   description: string;
 
-  @Column({type:'tinyint', default: false})
+  @Column({ type: 'tinyint', default: false })
   isChoice: boolean;
 
-  @ManyToOne(type=> QuizBookEntity, quizBook => quizBook.quizs)
-  @JoinColumn({name: 'quizBookId', referencedColumnName: 'id'})
-  quizBook: QuizBookEntity;
+  @ManyToOne(() => QuizBookEntity, (quizBook) => quizBook.quizs)
+  @JoinColumn({ name: 'quizBookId', referencedColumnName: 'id' })
+  quizBookId: number;
 
-  @OneToMany(type=>UserSolveQuizBookEntity, save=>save.savedQuiz)
+  @OneToMany(() => UserSolveQuizBookEntity, (save) => save.savedQuiz)
   saves: UserSolveQuizBookEntity[];
-
 }
