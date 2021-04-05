a3450a032ccd96cfa05166b497cdc8153ba250e0
namda-on <jinyoung1997@gmail.com>
2021-04-05 20:33:16 +0900

    fix : google login error 해결
    dto 코드 오류 수정

--- src/auth/dto/google-user.dto.ts
+++ src/auth/dto/google-user.dto.ts
@@ -5,9 +5,10 @@ export default class GoogleUserDTO {
   name: string;
 
   constructor(userData: AxiosResponse) {
-    email: userData.data.email;
-    name: userData.data.name.length > 10
-      ? userData.data.name.slice(0, 10)
-      : userData.data.name;
+    this.email = userData.data.email;
+    this.name =
+      userData.data.name.length > 10
+        ? userData.data.name.slice(0, 10)
+        : userData.data.name;
   }
 }
